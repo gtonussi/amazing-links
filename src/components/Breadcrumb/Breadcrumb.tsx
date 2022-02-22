800b8e5842ce98e175a683cf01cb9e102922ee65
Giovanni Tonussi <itm_gtonussi@uolinc.com>
2022-02-22 16:40:58 -0300

    feat: Improvements on GA and Breadcrumbs

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -3,18 +3,16 @@ import Link from 'next/link'
 import * as I from './Breadcrumb.interfaces'
 import * as S from './Breadcrumb.styles'
 
-export const Breadcrumb: React.FC<I.BreadcrumbProps> = ({ creator }) => {
-  return (
-    <S.Breadcrumb__Container>
-      <p>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </p>
-      <p>|</p>
-      <p>
-        <strong>{creator}</strong>
-      </p>
-    </S.Breadcrumb__Container>
-  )
-}
+export const Breadcrumb: React.FC<I.BreadcrumbProps> = ({ creator }) => (
+  <S.Breadcrumb__Container>
+    <p>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </p>
+    <p>|</p>
+    <p>
+      <strong>{creator}</strong>
+    </p>
+  </S.Breadcrumb__Container>
+)
