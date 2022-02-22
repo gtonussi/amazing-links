a9f04d2ea885dab724acf0cfec2f74fc43f22b55
Giovanni Tonussi <itm_gtonussi@uolinc.com>
2022-02-22 14:17:12 -0300

    feat(Analytics): Implements Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,32 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
-import { GlobalStyle } from '../styles/global'
-import theme from '../styles/theme'
+import { Analytics } from 'containers'
+import { GlobalStyle } from 'styles/global'
+import theme from 'styles/theme'
+import * as gtag from 'services/gtag'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    //https://willianjusten.com.br/como-configurar-o-google-analytics-no-nextjs-em-2021
+
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
       <GlobalStyle />
     </ThemeProvider>
   )
